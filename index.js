f2d4f63bc16aa7580c0504b5ecd71614e2c22c4a
Shawn Mathew <shawn.mathew@agencychief.com>
2021-06-08 07:34:53 -0400

    1.1.0

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
-clearConsole();
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+welcome({
+  title: pkgJSON.name,
+  tagLine: `Hey, I'm Shawn Mathew`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#fadc00`,
+  color: `#000`,
+  bold: true,
+  clear: true
+});
 
 console.log(`
-Shawn Mathew - Software Developer
+Shawn Mathew - Frontend Engineer
 
-Some Info
-Some more info
+I'm a frontend engineer building web experiences for both public and private customers. 
+I collaborate with engineers, product managers and designers to deliver frontend systems that meet the need of its users.
+
+Tech I work with:
+Javascript, Typescript, React, Redux, Node.js, Next.js, GraphQL, Apollo, Sass, Webpack, Babel, NPM, Jest, Enzyme
 
 `);
